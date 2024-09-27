b9c256174d3fe889178dd7c390899e0d8811f438
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-09-27 19:53:55 -0300

    atualização app-pizzaria-react-native

--- scr/services/api.ts
+++ scr/services/api.ts
@@ -6,8 +6,10 @@ import axios from "axios";
 const api = axios.create({
 
   // baseURL: 'http://localhost:3333'
-  baseURL: 'http://192.168.0.4:3333'
-  // baseURL: 'https://pizzaria-frontendbuild.vercel.app/'
+  // baseURL: 'http://192.168.0.4:3333'
+  baseURL: 'https://paizzaria-backend-build-nine.vercel.app/'
+
+  
 })
 
 export { api };
